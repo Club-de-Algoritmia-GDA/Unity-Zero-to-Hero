3fc6c8091edf1017d041e96173a6292a0c46175a
S4NDIX <sandix390@gmail.com>
2020-08-26 15:34:09 -0500

    Prototype v2 done

---
+++ Assets/Scripts/HorizontalMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class HorizontalMove : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float direction;
+    [SerializeField]private float speed = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        userInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move(new UnityEngine.Vector3(0, 0, direction));
+    }
+
+    private void userInput()
+    {
+        direction = Input.GetAxis("Horizontal");
+    }
+
+    private void Move(UnityEngine.Vector3 movement)
+    {
+        rb.AddForce(movement*speed);
+    }
+}
---
+++ Assets/Scripts/VerticalMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class VerticalMove : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float direction;
+    [SerializeField]private float speed = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        userInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move(new UnityEngine.Vector3(direction, 0,0));
+    }
+
+    private void userInput()
+    {
+        direction = Input.GetAxis("Vertical");
+    }
+
+    private void Move(UnityEngine.Vector3 movement)
+    {
+        rb.AddForce(movement*speed*-1);
+    }
+}
